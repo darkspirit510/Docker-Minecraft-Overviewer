a15551a6af4b96e17f911d229be20ac40651d96e
Sascha Schmidt <sascha.schmidt91@gmx.de>
2020-04-08 19:27:23 +0200

    inject slider plugin

--- scheduled_creator.sh
+++ scheduled_creator.sh
@@ -4,6 +4,9 @@ while true; do
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py --genpoi
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
 
+  # Add JS magic to map
+  sed -i '/<script src="leaflet.js"><\/script>/a <script src="https://unpkg.com/leaflet.marker.slideto@0.2.0/Leaflet.Marker.SlideTo.js"><\/script>' /tmp/export/index.html
+
   echo "Sleeping 1h... 🥱"
   sleep 1h
 done
