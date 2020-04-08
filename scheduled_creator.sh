f00f18daf8f7d4bb0d88f5b29250a870ab9b1fae
Sascha Schmidt <sascha.schmidt91@gmx.de>
2020-04-08 20:07:05 +0200

    run additional script - test

--- scheduled_creator.sh
+++ scheduled_creator.sh
@@ -4,8 +4,16 @@ while true; do
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py --genpoi
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
 
-  # Add JS magic to map
+  echo "Extending map scripts 🚀"
+  cat <<EOT > map_magic.js
+function map_magic() {
+  alert("TEST");
+}
+EOT
+
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="https://unpkg.com/leaflet.marker.slideto@0.2.0/Leaflet.Marker.SlideTo.js"><\/script>' /tmp/export/index.html
+  sed -i '/<script src="leaflet.js"><\/script>/a <script src="map_magic.js"><\/script>' /tmp/export/index.html
+  sed -i '/overviewer.util.initialize()/a ; map_magic() ;' /tmp/export/index.html
 
   echo "Sleeping 1h... 🥱"
   sleep 1h
