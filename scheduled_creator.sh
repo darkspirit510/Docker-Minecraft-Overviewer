fcbd2ca35bb8152a0c5562071af360123aa0b384
Sascha Schmidt <sascha.schmidt91@gmx.de>
2020-04-08 21:27:47 +0200

    move script to file

--- scheduled_creator.sh
+++ scheduled_creator.sh
@@ -5,32 +5,12 @@ while true; do
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
 
   echo "Extending map scripts 🚀"
-  cat <<EOT >/tmp/export/map_magic.js
-function map_magic() {
-  var xmlhttp = new XMLHttpRequest();
-
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        JSON.parse(this.responseText).forEach(player => {
-        var greenIcon = L.icon({
-          iconUrl: 'https://crafatar.com/avatars/' + player.uuid,
-          iconSize: [32, 32],
-        });
-        L.marker(overviewer.util.fromWorldToLatLng(player.x, player.y, player.z, conf), {icon: greenIcon})
-          .addTo(overviewer.map)
-          .bindTooltip(player.name);
-        });
-      }
-    }
-  };
-  xmlhttp.open("GET", "player_positions.json", true);
-  xmlhttp.send();
-}
-EOT
 
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="https://unpkg.com/leaflet.marker.slideto@0.2.0/Leaflet.Marker.SlideTo.js"><\/script>' /tmp/export/index.html
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="map_magic.js"><\/script>' /tmp/export/index.html
   sed -i -e 's/overviewer.util.initialize()/overviewer.util.initialize();map_magic();/g' /tmp/export/index.html
+
+  cp map_magic.js /tmp/export/
 
   echo "Sleeping 1h... 🥱"
   sleep 1h
