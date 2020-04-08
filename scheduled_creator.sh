67041d3cbe7e6c50d1aff3ef7e5df401ae45d672
Sascha Schmidt <sascha.schmidt91@gmx.de>
2020-04-08 20:26:36 +0200

    fix init function replacement

--- scheduled_creator.sh
+++ scheduled_creator.sh
@@ -5,7 +5,7 @@ while true; do
   /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
 
   echo "Extending map scripts 🚀"
-  cat <<EOT > map_magic.js
+  cat <<EOT > /tmp/export/map_magic.js
 function map_magic() {
   alert("TEST");
 }
@@ -13,7 +13,7 @@ EOT
 
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="https://unpkg.com/leaflet.marker.slideto@0.2.0/Leaflet.Marker.SlideTo.js"><\/script>' /tmp/export/index.html
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="map_magic.js"><\/script>' /tmp/export/index.html
-  sed -i '/overviewer.util.initialize()/a ; map_magic() ;' /tmp/export/index.html
+  sed -i -e 's/overviewer.util.initialize()/overviewer.util.initialize();map_magic();/g' /tmp/export/index.html
 
   echo "Sleeping 1h... 🥱"
   sleep 1h
