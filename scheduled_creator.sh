5d23a7d4dae95ab1108c18bf218bd7966395bea9
Sascha Schmidt <sascha.schmidt91@gmx.de>
2020-04-10 12:38:26 +0200

    fix missing positions after poi generation

--- scheduled_creator.sh
+++ scheduled_creator.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-while true; do
-  /tmp/overviewer/overviewer.py --config=/tmp/config/config.py --genpoi
-  /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
-
-  echo "Extending map scripts 🚀"
+apply_map_magic() {
+    echo "Extending map scripts 🚀"
   cat <<EOT >/tmp/export/map_magic.js
 function formatDate(milliseconds) {
   const d = new Date(milliseconds);
@@ -38,6 +35,14 @@ EOT
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="https://unpkg.com/leaflet.marker.slideto@0.2.0/Leaflet.Marker.SlideTo.js"><\/script>' /tmp/export/index.html
   sed -i '/<script src="leaflet.js"><\/script>/a <script src="map_magic.js"><\/script>' /tmp/export/index.html
   sed -i -e 's/overviewer.util.initialize()/overviewer.util.initialize();map_magic();/g' /tmp/export/index.html
+}
+
+while true; do
+  /tmp/overviewer/overviewer.py --config=/tmp/config/config.py --genpoi
+  apply_map_magic
+
+  /tmp/overviewer/overviewer.py --config=/tmp/config/config.py
+  apply_map_magic
 
   echo "Sleeping 1h... 🥱"
   sleep 1h
